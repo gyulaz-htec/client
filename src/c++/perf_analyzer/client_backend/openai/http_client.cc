7c5b65178d251e26cb996698aeca3a8092faaa7a
David Yastremsky <dyastremsky@nvidia.com>
2024-05-29 18:10:16 -0700

    Save current solution. Highly performant but hangs.

--- src/c++/perf_analyzer/client_backend/openai/http_client.cc
+++ src/c++/perf_analyzer/client_backend/openai/http_client.cc
@@ -1,29 +1,3 @@
-// Copyright 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-//  * Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-//  * Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-//  * Neither the name of NVIDIA CORPORATION nor the names of its
-//    contributors may be used to endorse or promote products derived
-//    from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
-// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 #include "http_client.h"
 
 #include <functional>
@@ -36,6 +10,7 @@ HttpRequest::HttpRequest(
     std::function<void(HttpRequest*)>&& completion_callback, const bool verbose)
     : completion_callback_(std::move(completion_callback)), verbose_(verbose)
 {
+  std::cout << "HttpRequest created" << std::endl;
 }
 
 HttpRequest::~HttpRequest()
@@ -44,6 +19,7 @@ HttpRequest::~HttpRequest()
     curl_slist_free_all(header_list_);
     header_list_ = nullptr;
   }
+  std::cout << "HttpRequest destroyed" << std::endl;
 }
 
 void
@@ -82,14 +58,6 @@ HttpClient::HttpClient(
     const HttpSslOptions& ssl_options)
     : url_(server_url), verbose_(verbose), ssl_options_(ssl_options)
 {
-  // [TODO TMA-1670] uncomment below and remove class-wise mutex once confirm
-  // curl >= 7.84.0 will always be used
-  // auto* ver = curl_version_info(CURLVERSION_NOW);
-  // if (ver->features & CURL_VERSION_THREADSAFE == 0) {
-  //   throw std::runtime_error(
-  //       "HTTP client has dependency on CURL library to have thread-safe "
-  //       "support (CURL_VERSION_THREADSAFE set)");
-  // }
   {
     std::lock_guard<std::mutex> lk(curl_init_mtx_);
     if (curl_global_init(CURL_GLOBAL_ALL) != 0) {
@@ -98,32 +66,37 @@ HttpClient::HttpClient(
   }
 
   multi_handle_ = curl_multi_init();
-
   worker_ = std::thread(&HttpClient::AsyncTransfer, this);
+  std::cout << "HttpClient created" << std::endl;
 }
 
 HttpClient::~HttpClient()
 {
   exiting_ = true;
-
-  // thread not joinable if AsyncInfer() is not called
-  // (it is default constructed thread before the first AsyncInfer() call)
+  std::cout << "Exiting HttpClient" << std::endl;
+  cv_.notify_all();
+  std::cout << "Notified all" << std::endl;
   if (worker_.joinable()) {
-    cv_.notify_all();
+    std::cout << "Joining worker thread" << std::endl;
     worker_.join();
   }
+  std::cout << "Worker thread joined" << std::endl;
 
-  for (auto& request : ongoing_async_requests_) {
-    CURL* easy_handle = reinterpret_cast<CURL*>(request.first);
-    curl_multi_remove_handle(multi_handle_, easy_handle);
-    curl_easy_cleanup(easy_handle);
+  while (!request_queue_.empty()) {
+    request_queue_.pop();
+    std::cout << "Request queue popped" << std::endl;
   }
-  curl_multi_cleanup(multi_handle_);
 
+  std::cout << "Request queue cleared" << std::endl;
+  curl_multi_cleanup(multi_handle_);
+  std::cout << "Multi handle cleaned up" << std::endl;
   {
     std::lock_guard<std::mutex> lk(curl_init_mtx_);
+    std::cout << "Lock guard acquired" << std::endl;
     curl_global_cleanup();
+    std::cout << "CURL global cleanup" << std::endl;
   }
+  std::cout << "HttpClient destroyed" << std::endl;
 }
 
 const std::string&
@@ -154,8 +127,7 @@ HttpClient::ParseSslKeyType(HttpSslOptions::KEYTYPE key_type)
       return der_str;
   }
   throw std::runtime_error(
-      "unsupported SSL key type encountered. Only PEM and DER are "
-      "supported.");
+      "Unsupported SSL key type encountered. Only PEM and DER are supported.");
 }
 
 void
@@ -183,16 +155,15 @@ HttpClient::SetSSLCurlOptions(CURL* curl_handle)
 void
 HttpClient::Send(CURL* handle, std::unique_ptr<HttpRequest>&& request)
 {
-  std::lock_guard<std::mutex> lock(mutex_);
-
-  auto insert_result = ongoing_async_requests_.emplace(
-      std::make_pair(reinterpret_cast<uintptr_t>(handle), std::move(request)));
-  if (!insert_result.second) {
-    curl_easy_cleanup(handle);
-    throw std::runtime_error(
-        "Failed to insert new asynchronous request context.");
+  std::cout << "Send called" << std::endl;
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    ongoing_async_requests_.emplace(
+        reinterpret_cast<uintptr_t>(handle), std::move(request));
+    curl_multi_add_handle(multi_handle_, handle);
+    std::cout << "Request added to ongoing_async_requests_" << std::endl;
   }
-  curl_multi_add_handle(multi_handle_, handle);
+  has_new_request_ = true;
   cv_.notify_all();
 }
 
@@ -201,76 +172,79 @@ HttpClient::AsyncTransfer()
 {
   int place_holder = 0;
   CURLMsg* msg = nullptr;
-  do {
+
+  while (true) {
     std::vector<std::unique_ptr<HttpRequest>> request_list;
 
-    // sleep if no work is available
-    std::unique_lock<std::mutex> lock(mutex_);
-    cv_.wait(lock, [this] {
-      if (this->exiting_) {
-        return true;
+    {
+      std::unique_lock<std::mutex> lock(mutex_);
+      cv_.wait(lock, [this] { return has_new_request_ || exiting_; });
+      if (exiting_ && ongoing_async_requests_.empty()) {
+        std::cout << "Exiting AsyncTransfer loop" << std::endl;
+        break;
       }
-      // wake up if an async request has been generated
-      return !this->ongoing_async_requests_.empty();
-    });
+      has_new_request_ = false;
+      std::cout << "New work detected, continuing AsyncTransfer" << std::endl;
+    }
 
     CURLMcode mc = curl_multi_perform(multi_handle_, &place_holder);
-    int numfds;
-    if (mc == CURLM_OK) {
-      // Wait for activity. If there are no descriptors in the multi_handle_
-      // then curl_multi_wait will return immediately
-      mc = curl_multi_wait(multi_handle_, NULL, 0, INT_MAX, &numfds);
-      if (mc == CURLM_OK) {
-        while ((msg = curl_multi_info_read(multi_handle_, &place_holder))) {
-          uintptr_t identifier = reinterpret_cast<uintptr_t>(msg->easy_handle);
-          auto itr = ongoing_async_requests_.find(identifier);
-          // This shouldn't happen
-          if (itr == ongoing_async_requests_.end()) {
-            std::cerr
-                << "Unexpected error: received completed request that is not "
-                   "in the list of asynchronous requests"
+    if (mc != CURLM_OK) {
+      std::cerr << "Unexpected error: curl_multi_perform failed. Code:" << mc
                 << std::endl;
-            curl_multi_remove_handle(multi_handle_, msg->easy_handle);
-            curl_easy_cleanup(msg->easy_handle);
-            continue;
-          }
+      continue;
+    }
 
-          uint32_t http_code = 400;
-          if (msg->data.result == CURLE_OK) {
-            curl_easy_getinfo(
-                msg->easy_handle, CURLINFO_RESPONSE_CODE, &http_code);
-          } else if (msg->data.result == CURLE_OPERATION_TIMEDOUT) {
-            http_code = 499;
-          }
+    int numfds;
+    mc = curl_multi_poll(multi_handle_, NULL, 0, 1000, &numfds);
+    if (mc != CURLM_OK) {
+      std::cerr << "Unexpected error: curl_multi_poll failed. Code:" << mc
+                << std::endl;
+      continue;
+    }
 
-          request_list.emplace_back(std::move(itr->second));
-          ongoing_async_requests_.erase(itr);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      while ((msg = curl_multi_info_read(multi_handle_, &place_holder))) {
+        uintptr_t identifier = reinterpret_cast<uintptr_t>(msg->easy_handle);
+        auto itr = ongoing_async_requests_.find(identifier);
+        if (itr == ongoing_async_requests_.end()) {
+          std::cerr
+              << "Unexpected error: received completed request that is not "
+              << "in the list of asynchronous requests" << std::endl;
           curl_multi_remove_handle(multi_handle_, msg->easy_handle);
           curl_easy_cleanup(msg->easy_handle);
-
-          std::unique_ptr<HttpRequest>& async_request = request_list.back();
-          async_request->http_code_ = http_code;
-
-          if (msg->msg != CURLMSG_DONE) {
-            // Something wrong happened.
-            std::cerr << "Unexpected error: received CURLMsg=" << msg->msg
-                      << std::endl;
-          }
+          continue;
         }
-      } else {
-        std::cerr << "Unexpected error: curl_multi failed. Code:" << mc
-                  << std::endl;
+
+        uint32_t http_code = 400;
+        if (msg->data.result == CURLE_OK) {
+          curl_easy_getinfo(
+              msg->easy_handle, CURLINFO_RESPONSE_CODE, &http_code);
+        } else if (msg->data.result == CURLE_OPERATION_TIMEDOUT) {
+          http_code = 499;
+        }
+
+        request_list.emplace_back(std::move(itr->second));
+        ongoing_async_requests_.erase(itr);
+        curl_multi_remove_handle(multi_handle_, msg->easy_handle);
+        curl_easy_cleanup(msg->easy_handle);
+
+        std::unique_ptr<HttpRequest>& async_request = request_list.back();
+        async_request->http_code_ = http_code;
+
+        if (msg->msg != CURLMSG_DONE) {
+          std::cerr << "Unexpected error: received CURLMsg=" << msg->msg
+                    << std::endl;
+        }
       }
-    } else {
-      std::cerr << "Unexpected error: curl_multi failed. Code:" << mc
-                << std::endl;
     }
-    lock.unlock();
 
     for (auto& this_request : request_list) {
       this_request->completion_callback_(this_request.get());
+      std::cout << "Completed request processed" << std::endl;
     }
-  } while (!exiting_);
+  }
+  std::cout << "AsyncTransfer exiting" << std::endl;
 }
 
 }}}}  // namespace triton::perfanalyzer::clientbackend::openai
